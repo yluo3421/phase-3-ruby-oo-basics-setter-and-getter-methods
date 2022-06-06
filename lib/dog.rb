7cbcef29927a6cd64954b475c0ade1db938c7a16
root <yluo3421@gmail.com>
2022-06-06 18:05:30 -0400

    commit tree 172ea24401acc7f0165b447a5d2cf9ba962ec4e5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # setter method
+    def name=(name)
+      @name = name
+    end
 
+    def breed=(breed)
+        @breed = breed
+      end
+  
+    def breed
+        @breed
+    end
+
+    # getter method
+    def name
+      @name
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
 
-end
+    # setter method
+    def name=(name)
+      @name = name
+    end
+  
+    def job=(job)
+        @job = job
+    end
+
+    # getter method
+    def name
+      @name
+    end
+
+    def job
+        @job
+    end
+  
+  end
